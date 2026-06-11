3f35d31d45494b126d3f023090289e9c3b558acd
Alizarine-musique <alizarinemusique@gmail.com>
2026-06-11 11:15:45 +0200

    l

--- aliza'fest/js/script.js
+++ aliza'fest/js/script.js
@@ -1,5 +1,4 @@
 const video = document.querySelector(".background-video");
-
 if (window.innerWidth <= 768) {
   if (video) {
     video.pause();
@@ -13,3 +12,30 @@ if (window.innerWidth <= 768) {
     video.load();
   }
 }
+
+const counters = document.querySelectorAll('.counter');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            const counter = entry.target;
+            const target = +counter.dataset.target;
+            let current = 0;
+            const increment = target / 100;
+            const timer = setInterval(() => {
+                current += increment;
+                if(current >= target){
+                    counter.textContent =
+                        target.toLocaleString('fr-FR') + ' €';
+                    clearInterval(timer);
+                } else {
+                    counter.textContent =
+                        Math.floor(current).toLocaleString('fr-FR') + ' €';
+                }
+            }, 20);
+            observer.unobserve(counter);
+        }
+    });
+});
+
+counters.forEach(counter => observer.observe(counter));
